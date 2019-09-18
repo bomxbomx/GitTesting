5a9d8f38d1cac844bc10caab58fd55b45442ae9e
Bomx <sammybomx@gmail.com>
2019-09-18 10:42:14 +0630

    new object instead of method

--- Javaconfiginjection/src/main/java/com/kmd/application/AppConfig.java
+++ Javaconfiginjection/src/main/java/com/kmd/application/AppConfig.java
@@ -14,7 +14,7 @@ public class AppConfig {
 	@Scope("prototype")
 	@Bean(name = "customerService")
 	public CustomerService getCustomerService() {
-		CustomerService service=new CustomerServiceImpl(getCustomerRepository());
+		CustomerService service=new CustomerServiceImpl(new CustomerRepositoryImpl());
 //		CustomerServiceImpl service = new CustomerServiceImpl();
 //		service.setCustomerRepo(new CustomerRepositoryImpl2());
 		return service;
